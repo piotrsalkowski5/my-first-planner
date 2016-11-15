09345c7fc60c2648017f35066d4f2e266a3cb3bc
sauron <piotrsalkowski5@gmail.com>
2016-11-15 20:41:39 +0100

    Zmieniony kod HTML na stronie.

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-
+import os, sys, datetime, time
+from django.utils import timezone
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -102,3 +102,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+
+
+
+
+
+
+
